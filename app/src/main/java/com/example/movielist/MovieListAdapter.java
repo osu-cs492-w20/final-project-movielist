9e2afeeb766371bea2ab054538b9ed4eb7132605
mbaiza27 <mbaiza27@outlook.com>
2020-03-10 21:35:22 -0700

    -Made "movie list item" layout this will hold the different lists we create that in turn holds different movies.
    - implemented recycler view adapter though some changes need to be made. Right now it is implemented with a dummy object "movie details." This will need to be change because that is the object that holds the details for individual movies. We need to implement a "movie list list" object that we can pass through the main recycler view.

--- app/src/main/java/com/example/movielist/MovieListAdapter.java
+++ app/src/main/java/com/example/movielist/MovieListAdapter.java
@@ -1,5 +1,6 @@
 package com.example.movielist;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -8,13 +9,45 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.MovieItemViewHolder> {
+import com.example.movielist.data.MovieDetails;
 
+import java.util.List;
+
+public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.MovieItemViewHolder> {
+    //All of our variables---------------------------------------------------------------------
+    private List<MovieDetails> mMovieDetailsItems;
+    private OnMovieDetailsItemClickListener mMovieDetailsItemClickListener;
+
+    //OnMovieDetailsItemClickListener Interface------------------------------------------------
+    public interface OnMovieDetailsItemClickListener{
+
+    }
+    //MovieAdapter (Implements Click Listener)-------------------------------------------------
+    public MovieListAdapter(OnMovieDetailsItemClickListener clickListener){
+        mMovieDetailsItemClickListener = clickListener;
+    }
+    //updateMovieDetailItems-------------------------------------------------------------------
+    public void updateMovieDetailItems(List<MovieDetails> movieDetailsItems){
+        mMovieDetailsItems = movieDetailsItems;
+        notifyDataSetChanged();
+    }
+    //Get Item Count---------------------------------------------------------------------------
+    //Waiting to get correct object implemented.
+    /*@Override
+    public int getItemCount() {
+        if (mForecastItems != null) {
+            return mForecastItems.size();
+        } else {
+            return 0;
+        }
+    }*/
     //ViewHolder "onCreate"--------------------------------------------------------------------
     @NonNull
     @Override
     public MovieItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View itemView = inflater.inflate(R.layout.movie_list_item, parent, false);
+        return new MovieItemViewHolder(itemView);
     }
     //onBindViewHolder--------------------------------------------------------------------------
     @Override
@@ -29,8 +62,8 @@ public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.Movi
     //MovieItemViewHolder------------------------------------------------------------------------
     class MovieItemViewHolder extends RecyclerView.ViewHolder{
 
-        private TextView mMovieNameTV;
-        private ImageView mMovieIconIV;
+        private TextView mMovieListTV;
+
 
         public MovieItemViewHolder(@NonNull View itemView) {
             super(itemView);
