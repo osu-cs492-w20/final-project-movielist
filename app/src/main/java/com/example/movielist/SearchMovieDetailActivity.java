87e651140be5d3cbf4961e7f1592021596c95bf1
Evan <amayae@oregonstate.edu>
2020-03-18 15:26:21 -0700

    - Fixed parent activity for SearchMovieDetailActivity - Created onclick to open intent to SearchMovieDetailActivity

--- app/src/main/java/com/example/movielist/SearchMovieDetailActivity.java
+++ app/src/main/java/com/example/movielist/SearchMovieDetailActivity.java
@@ -3,13 +3,16 @@ package com.example.movielist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class SearchMovieDetailActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MOVIE_DETAIL = "MovieDetail";
+    private String TAG = SearchMovieDetailActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_movie_detail);
+        Log.d(TAG,"In ACTIVITY NOW");
 
 
     }
