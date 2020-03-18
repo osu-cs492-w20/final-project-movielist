c421a3a076b3526bacbe160efa05bfaec0785a11
Evan <amayae@oregonstate.edu>
2020-03-18 15:04:45 -0700

    - Created SearchMovieDetailActivity/Layout file - removed extra text view in SearchActivity - Added parent activity so back button goes back to SearchActivity

---
+++ app/src/main/java/com/example/movielist/SearchMovieDetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.movielist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchMovieDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_movie_detail);
+
+
+    }
+}
